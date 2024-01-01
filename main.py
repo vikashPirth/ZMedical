67519ea1374d117963a62451545be4e9ec511e8d
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-01 22:19:11 +0500

    update the rest of functions

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
 welcome_prompt = "Welcome Doctor! What would you like to do today?\n - To List all patients press 1. \n To run a diagnosis press 2.\n To press quit q.\n"
 name_prompt  = "What is the patient's name?"
 appearence_prompt = "How is the patient's general apperance?\n  1. Normal appeearnce\n 2. Irritable or lethargic\n"
+eye_prompt = "How are the patient's eyes?\n  1. Eves Normal or Slightly sunken\n 2. Eyes really Sunken\n"
+skin_promopt = "How is the patient's skin when you pinch?\n  1.Normal skin pinch \n 2.Slow skin pinch.\n"
+sever_dehydration = "Server Dehydration"
+some_dehydration  = "Some Dehydration"
+no_dehydration = "No Dehydration"
+error_message = "Could not save the patients diagnosis due to invalid input"
 
+patients_and_diagnosis = [
 
-def assess_eyes():
-    print("Ask the question for eyes")
+    "Mike: Severe Dehydration", 
+    "Vikash: No Dehydration",
+    "Ali: Some Dehydration"
+]
 
-def assess_skin():
-    print("ask the question for skin")
+def assess_eyes(eyes: str):
+    if eyes == "1" :
+        return no_dehydration
+    elif eyes == "2":
+        return sever_dehydration
+    else:
+        return ""
+
+def assess_skin(skin: str):
+    if skin == "1" :
+        return some_dehydration
+    elif skin == "2":
+        return sever_dehydration
+    else:
+        return ""
 
 def list_patients():
-    print("List all patients and diagnosis")
+    for patient in patients_and_diagnosis:
+        print(patient)
+
+def save_new_diagnosis(name, diagnosis):
+    if name == "" or diagnosis == "":
+        print(error_message)
+        return
+    final_diagnosis = f'{name}:{diagnosis}'
+    patients_and_diagnosis.append(final_diagnosis)
+    print("Final diagnosis:" , final_diagnosis, "\n")
+
+def access_appearence():
+    appearence =  input(appearence_prompt)
+    if appearence == "1" :
+        eyes = input(eye_prompt)
+        return assess_eyes(eyes=eyes)
+    elif appearence == "2":
+        skin = input(skin_promopt)
+        return assess_skin(skin=skin)
+    else:
+        return ""
 
 def start_new_diagnosis():
     name = input(name_prompt)
-    appearence =  input(appearence_prompt)
+    diagnosis = access_appearence()
+    save_new_diagnosis(name=name, diagnosis=diagnosis)
     
-    if appearence == "1" :
-        assess_eyes()
-    elif appearence == "2":
-        assess_skin()
-    else:
-        print("input is not valid")
-
-
-
-def main():
+def main(): 
    
    while (True):
         selection = input(welcome_prompt)
